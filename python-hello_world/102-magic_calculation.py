d7fb531eb9ab9971795fd5790d0ef1d8ca7f3fec
boris-ny <b.nyilindek@alustudent.com>
2022-10-17 22:08:00 +0200

    trial

--- python-hello_world/102-magic_calculation.py
+++ python-hello_world/102-magic_calculation.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
+#Write the Python function def magic_calculation(a, b): that does exactly the same as the following Python bytecode:
+
+  #3           0 LOAD_CONST               1 (98)
+  #            3 LOAD_FAST                0 (a)
+  #            6 LOAD_FAST                1 (b)
+  #            9 BINARY_POWER
+  #           10 BINARY_ADD
+  #           11 RETURN_VALUE
+
 def magic_calculation(a, b):
     return len(a) + len(b)
 def dis.dis(magic_calculation):
-     3        0 LOAD_CONST               1 (98)
-              3 LOAD_FAST                0 (a)
-              6 LOAD_FAST                1 (b)
-              9 BINARY_POWER
-             10 BINARY_ADD
-             11 RETURN_VALUE
+    3           0 LOAD_CONST               1 (98)
+    3           3 LOAD_FAST                0 (a)
+    3           6 LOAD_FAST                1 (b)
+    3           9 BINARY_POWER
+    3          10 BINARY_ADD
+    3          11 RETURN_VALUE
+    
