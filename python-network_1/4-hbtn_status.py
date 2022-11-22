af873117436294d93327e94a924f2e4c10fa9401
boris-ny <b.nyilindek@alustudent.com>
2022-11-22 21:43:33 +0200

    python network 🦜

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -2,7 +2,7 @@
 '''Module 4-hbtn_status.py'''
 import requests
 if __name__ == "__main__":
-   html = requests.get("https://intranet.hbtn.io/status")
-   print("Body response:")
-   print("\t- type: {}".format(type(html.text)))
-   print("\t- content: {}".format(html.text))
+    html = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(html.text)))
+    print("\t- content: {}".format(html.text))
