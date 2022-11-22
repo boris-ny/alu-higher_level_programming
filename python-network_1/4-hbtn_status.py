f660498a49b784ba3757d98817c11686be16c2e6
boris-ny <b.nyilindek@alustudent.com>
2022-11-22 21:35:11 +0200

    python network 🦜

---
+++ python-network_1/4-hbtn_status.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Module 4-hbtn_status.py'''
+import urllib.request
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
