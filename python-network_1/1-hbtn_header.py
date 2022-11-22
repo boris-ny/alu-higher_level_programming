a42924969d3b92901aae8765081efbff8ddd7b0e
boris-ny <b.nyilindek@alustudent.com>
2022-11-22 21:19:58 +0200

    python network 🦜

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-'''Python script that takes in a URL, sends a request to the URL and displays the value of the X-Request-Id variable.'''
+'''X-Request-Id variable in header.'''
 import urllib.request
 import sys
 if __name__ == "__main__":
