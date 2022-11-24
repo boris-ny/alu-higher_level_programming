a3911d4a0303a7de718871e31bedc27c8fd2f658
boris-ny <b.nyilindek@alustudent.com>
2022-11-24 16:15:53 +0200

    Sequential solutions 🕸️

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,12 +1,12 @@
 -- create a table called second_table in the database hbtn_0c_0
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
-    name VARCHAR(256),
+    name VARCHAR(25),
     score INT,
 );
 
 INSERT INTO second_table (id, name, score) 
 VALUES (1, 'John', 10),
-(2, 'Alex', 3),
-(3, 'Bob', 14),
-(4, 'George', 8);
+ (2, 'Alex', 3),
+ (3, 'Bob', 14),
+ (4, 'George', 8);
