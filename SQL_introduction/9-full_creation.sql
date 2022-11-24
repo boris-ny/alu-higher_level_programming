b7007041dd42569a35a9b9a39751e2e0652497f2
boris-ny <b.nyilindek@alustudent.com>
2022-11-24 16:25:50 +0200

    Sequential solutions 🕸️

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,8 +1,3 @@
--- create a table called second_table in the database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (
-    id INT,
-    name VARCHAR(25),
-    score INT,
-);
-
-INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
+-- create a table with multiple rows
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10), (2, 'Alex', 3), (3, 'Bob', 14), (4, 'George', 8);
