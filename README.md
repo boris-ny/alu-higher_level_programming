e54d331385bc02da8ef75a373bb917c772b7615c
boris-ny <b.nyilindek@alustudent.com>
2022-12-06 12:34:44 +0200

    python summative 🕸️

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # **Alu-Higher_Level_Programming**
+
 >## **ALU repo for Python, SQL and related exercises 🐍**
 
 >- [**python-hello_world**](python-hello_world)
@@ -6,3 +7,14 @@
 >- [**python-import_modules**](python-import_modules)
 >- [**python-data_structures**](python-data_structures)
 >- [**python-more_data_structures**](python-more_data_structures)
+>- [**python-classes**](python-classes)
+>- [**python-exceptions**](python-exceptions)
+>- [**python-almost_a_circle**](python-almost_a_circle)
+>- [**SQL_introduction**](SQL_introduction)
+>- [**SQL_more_queries**](SQL_more_queries)
+>- [**python-network_0**](python-network_0)
+>- [**python-network_1**](python-network_1)
+>- [**python-input_output**](python-input_output)
+>- [**python-inheritance**](python-inheritance)
+>- [**python-everything_is_object**](python-everything_is_object)
+>- [**python-more_classes**](python-more_classes)
---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+"""Base class for all models in this project"""
+
+import json
+
+class Base:
+    '''
+    Class Base Attributes: id number
+    '''
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        '''
+        Returns the JSON string of list_dictionaries
+        '''
+
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        ''''Writes the JSON string representation of list_objs to a file'''
+
+        filename = cls.__name__ + ".json"
+        list_dict = []
+
+        if list_objs is not None:
+            for obj in list_objs:
+                list_dict.append(obj.to_dictionary())
+
+        with open(filename, 'w') as f:
+            f.write(cls.to_json_string(list_dict))
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''Returns the list of the JSON string representation json_string'''
+
+        if json_string is None or json_string == []:
+            return []
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''Returns an instance with all attributes already set'''
+
+        if cls.__name__ == "Rectangle":
+            dummy = cls(1, 1)
+        else:
+            dummy = cls(1)
+        dummy.update(**dictionary)
+        return dummy
+
+    @classmethod
+    def load_from_file(cls):
+        '''Returns a list of instances'''
+
+        filename = cls.__name__ + ".json"
+        list_instances = []
+
+        try:
+            with open(filename, 'r') as f:
+                list_dict = cls.from_json_string(f.read())
+                for dict in list_dict:
+                    list_instances.append(cls.create(**dict))
+                return list_instances
+        except:
+            return []
