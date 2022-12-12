36425f6cf3e708733a7baa2a548c72278beb8beb
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 15:34:35 +0200

    relational mapping 🐬

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-""" A script that takes 
-in the name of a state as an 
-argument and lists all cities of 
+""" A script that takes
+in the name of a state as an
+argument and lists all cities of
 that state, using the database hbtn_0e_4_usa"""
 
 import MySQLdb
@@ -15,7 +15,7 @@ if __name__ == "__main__":
         password=argv[2],
         db=argv[3])
 
-    ora=db_get.cursor()
+    ora = db_get.cursor()
     ora.execute("SELECT cities.id, cities.name, states.name FROM cities \
     JOIN states ON cities.state_id = states.id ORDER BY cities.id")
     
