56f49fcabe7ac83678856bf8ce17611318016740
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 13:10:11 +0200

    relational mapping 🐬

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,12 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db_get= MySQLdb.connect(host="localhost", port=3306, user=argv[1],password=argv[2], db=argv[3], charset="utf8")
+    db_get= MySQLdb.connect(host="localhost", 
+    port=3306, 
+    user=argv[1], 
+    password=argv[2], 
+    db=argv[3], 
+    charset="utf8")
 
     ora=db_get.cursor()
     ora.execute("SELECT * FROM states ORDER BY id ASC")
