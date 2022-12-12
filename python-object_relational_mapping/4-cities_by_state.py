5d1f68aa82fa7f65315aaf3d57ed8d3140fa9922
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 15:30:45 +0200

    relational mapping 🐬

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     ora=db_get.cursor()
     ora.execute("SELECT cities.name FROM \
         cities INNER JOIN states ON cities.state_id = states.id WHERE states.name = %s \
-            ORDER BY cities.id ASC", (argv[4],))
+            ORDER BY cities.id ASC")
     
     for row in ora.fetchall():
         print(row)
