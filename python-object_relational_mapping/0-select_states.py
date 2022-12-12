d965c2aaf0d72207efa3c419c257df77373154ec
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 13:13:23 +0200

    relational mapping 🐬

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -7,15 +7,15 @@ from sys import argv
 
 if __name__ == "__main__":
     db_get= MySQLdb.connect(host="localhost", 
-    port=3306, 
-    user=argv[1], 
-    password=argv[2], 
-    db=argv[3], 
+    port=3306,
+    user=argv[1],
+    password=argv[2],
+    db=argv[3],
     charset="utf8")
 
     ora=db_get.cursor()
     ora.execute("SELECT * FROM states ORDER BY id ASC")
-    
+
     for row in ora.fetchall():
         print(row)
     ora.close()
