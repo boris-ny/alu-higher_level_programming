077f5cf622f1abaef5f0648f74fd4497cc8e72d2
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 15:35:10 +0200

    relational mapping 🐬

--- python-object_relational_mapping/4-cities_by_state.py
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     ora = db_get.cursor()
     ora.execute("SELECT cities.id, cities.name, states.name FROM cities \
     JOIN states ON cities.state_id = states.id ORDER BY cities.id")
-    
+
     for row in ora.fetchall():
         print(row)
     ora.close()
