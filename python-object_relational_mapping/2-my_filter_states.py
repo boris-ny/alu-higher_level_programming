714ffa9edcbc1e1e18b77072a2018258d8b8d577
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 15:03:11 +0200

    relational mapping 🐬

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" A script that takes in an argument and displays all values in the states table."""
+""" A script that takes in an argument and displays all values in the \
+    states table."""
 
 import MySQLdb
 from sys import argv
