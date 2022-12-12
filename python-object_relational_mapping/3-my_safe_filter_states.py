0dfe038caa387a936e73a6ecf651cc14abc60c83
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 15:19:11 +0200

    relational mapping 🐬

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-""" A script that takes in an argument and displays all values in the states table \
+""" A script that takes \
+    in an argument and displays all values in the states table \
     of hbtn_0e_0_usa where name matches the argument."""
 
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db_get = MySQLdb.connect(host="localhost", port=3306, user=argv[1], password=argv[2], db=argv[3])
+    db_get = MySQLdb.connect(
+        host="localhost",
+        port=3306, user=argv[1],
+        password=argv[2],
+        db=argv[3])
 
     ora = db_get.cursor()
     ora.execute("SELECT * FROM states WHERE name LIKE BINARY %s \
