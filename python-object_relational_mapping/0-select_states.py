10a2e6a9be072730619be2a5ce80062d5eb9d5b2
boris-ny <b.nyilindek@alustudent.com>
2022-12-12 12:39:23 +0200

    relational mapping 🐬

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" A script that lists all states from the database hbtn_0e_0_usa"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db_get= MySQLdb.connect(host="localhost", port=3306, user=argv[1],password=argv[2], db=argv[3])
+
+    ora=db_get.cursor()
+    ora.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in ora.fetchall():
+        print(row)
+    ora.close()
+    db_get.close()
