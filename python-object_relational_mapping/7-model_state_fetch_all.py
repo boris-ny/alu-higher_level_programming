6237d5b8bc4b493dd9c222c5817afebc3743d18d
boris-ny <b.nyilindek@alustudent.com>
2022-12-13 14:52:56 +0200

    relational mapping 🐬

---
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""A script that lists all State objects from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+if __name__ == "__main__":
+    jid = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+
+    Base.metadata.create_all(jid)
+    yeat = sessionmaker(bind=jid)
+    session = yeat()
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
+    session.close()
