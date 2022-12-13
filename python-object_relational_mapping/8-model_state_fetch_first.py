b45c0f3be6ebfe0a8d403344da5ecd8b0064d29d
boris-ny <b.nyilindek@alustudent.com>
2022-12-13 14:59:55 +0200

    relational mapping 🐬

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""A script that prints the first State object from the database hbtn_0e_6_usa"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    jid = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1],
+                                                                    argv[2],
+                                                                    argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(jid)
+    yeat = sessionmaker(bind=jid)
+    session = yeat()
+    state = session.query(State).first()
+    if state:
+        print("{}: {}".format(state.id, state.name))
+    else:
+        print("Nothing")
+    session.close()
