1392cee929bd2185930a9872ba651677c84657ed
boris-ny <b.nyilindek@alustudent.com>
2023-02-07 15:11:06 +0200

    JS🐍🥲

---
+++ javascript-web_scraping/0-readme.js
@@ -0,0 +1,11 @@
+#!/usr/nin/node
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', (err, data) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
+    console.log(data);
+});
---
+++ javascript-web_scraping/README.md
@@ -0,0 +1,3 @@
+# JAVASCRIPT-WEB_SCRAPING
+
+## Description: This repository contains a collection of web scraping scripts written in JavaScript.
