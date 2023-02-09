4612b035376b778c8b60bc673819a8b93dce485d
boris-ny <b.nyilindek@alustudent.com>
2023-02-09 10:30:17 +0200

    JS🐍🥲

--- javascript-web_scraping/3-starwars_title.js
+++ javascript-web_scraping/3-starwars_title.js
@@ -3,5 +3,5 @@ const request = require('request');
 const url = process.argv[2];
 request.get(`https://swapi-api.alx-tools.com/api/films/:id/${url}`, (error, response) => {
   if (error) { console.log(error); }
-  console.log(JSON.parse(response.body).title);
+  console.log(JSON.parse(response).title);
 });
